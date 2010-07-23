2fed826c9bf4887fc18fbaa66f3d1d97202b51ca
Victor Karamzin <visor@alt22.ru>
2010-07-24 02:25:36 +0700

    * load plugins from external file for each executed controller * add plugins and library folders for application

--- application/settings/routes.php
+++ application/settings/routes.php
@@ -2,4 +2,8 @@
 
 Nano::routes()
 	->add('^$', 'index', 'index')
+
+	->add('^cp$',                  'control-panel', 'dashboard')
+	->add('^cp/items$',            'control-panel', 'items')
+	->add('^cp/edit/(?P<id>\d+)$', 'control-panel', 'edit')
 ;
--- library/Nano/C/Plugin.php
+++ library/Nano/C/Plugin.php
@@ -2,8 +2,22 @@
 
 interface Nano_C_Plugin {
 
+	/**
+	 * @return void
+	 * @param Nano_C $controller
+	 */
+	public function init(Nano_C $controller);
+
+	/**
+	 * @return boolean
+	 * @param Nano_C $controller
+	 */
 	public function before(Nano_C $controller);
 
+	/**
+	 * @return void
+	 * @param Nano_C $controller
+	 */
 	public function after(Nano_C $controller);
 
 }
