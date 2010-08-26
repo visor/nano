8fd279829f724a2ac5c5e34646ca71082e1e9824
Victor Karamzin <visor@alt22.ru>
2010-08-26 12:53:33 +0700

    cosmetics

--- application/views/control-panel/items.php
+++ application/views/control-panel/items.php
@@ -62,13 +62,13 @@
 				<div class="pagination">
 					<span class="previous-off">&laquo; Previous</span>
 					<span class="active">1</span>
-					<a href="dashboard4658.html?page=2">2</a>
-					<a href="dashboard9ba9.html?page=3">3</a>
-					<a href="dashboardfdb0.html?page=4">4</a>
-					<a href="dashboardaf4d.html?page=5">5</a>
-					<a href="dashboardc575.html?page=6">6</a>
-					<a href="dashboard235c.html?page=7">7</a>
-					<a href="dashboard4658.html?page=2" class="next">Next &raquo;</a>
+					<a href="?page=2">2</a>
+					<a href="?page=3">3</a>
+					<a href="?page=4">4</a>
+					<a href="?page=5">5</a>
+					<a href="?page=6">6</a>
+					<a href="?page=7">7</a>
+					<a href="?page=2" class="next">Next &raquo;</a>
 				</div>
 			</td>
 		</tr>
