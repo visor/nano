4af2c2a584b8a9657278afe82ec88bdb3d87bddc
Victor Karamzin <visor@alt22.ru>
2010-09-09 10:46:22 +0700

    add cookie utility class

---
+++ library/Cookie.php
@@ -0,0 +1,48 @@
+<?php
+
+class Cookie {
+
+	const ONE_MONTH = 2592000;
+
+	/**
+	 * @return scalar
+	 * @param string $name
+	 * @param scalar $default
+	 */
+	public static function get($name, $default = null) {
+		if (isset($_COOKIE[$name])) {
+			return $_COOKIE[$name];
+		}
+		return $default;
+	}
+
+	/**
+	 * @return void
+	 * @param string $name
+	 * @param scalar $value
+	 * @param int $expire
+	 */
+	public static function set($name, $value, $expire = null) {
+		if (null === $expire) {
+			$expire = ONE_MONTH;
+		}
+		setCookie($name, $value, $_SERVER['REQUEST_TIME'] + $expire, '/', self::domain(), true, true);
+		$_COOKIE[$name] = $value;
+	}
+
+	/**
+	 * @return void
+	 * @param string $name
+	 */
+	public static function erase($name) {
+		setCookie($name, null, 0, '/', self::domain(), true, true);
+		unset($_COOKIE[$name]);
+	}
+
+	/**
+	 * @return string
+	 */
+	protected static function domain() {
+		return '.' . Nano::config('web')->domain;
+	}
+}
