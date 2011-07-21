a9f2186771f6de07fab312f02b4ce5931d91766e
Victor Karamzin <admin@visor.ws>
2011-07-21 16:28:16 +0700

    fix configuration path

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
 Nano_Config::setFormat(new Nano_Config_Format_Json());
-Nano::configure(new Nano_Config(APP . DS . Nano_Config::CONFIG_FILE_NAME));
+Nano::configure(new Nano_Config(SETTINGS . DS . Nano_Config::CONFIG_FILE_NAME));
 Nano::instance();
