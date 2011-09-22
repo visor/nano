548d1d582090f77c1fa72cb9dd16e2d959769427
Victor Karamzin <admin@visor.ws>
2011-09-22 18:56:42 +0700

    Add empty deploy script (todo)

---
+++ scripts/deploy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @description Update current GIT branch, run setup and clean assets
+ */
+
+namespace CliScript;
+
+class Deploy extends \Nano_Cli_Script {
+
+	/**
+	 * @param string[] $args
+	 * @return void
+	 */
+	public function run(array $args) {
+		echo 'Not implemented yet';
+	}
+
+}
