f1b82b7569d1e09fd5130dc807d8fa41afac0936
Victor Karamzin <admin@visor.ws>
2011-11-24 18:50:52 +0700

    * Start clears ControllerTestCase class

--- library/TestUtils/ControllerTestCase.php
+++ library/TestUtils/ControllerTestCase.php
@@ -12,62 +12,62 @@ class TestUtils_ControllerTestCase extends TestUtils_TestCase {
 	 */
 	protected $clearDatabase = false;
 
-	/**
-	 * @return string
-	 * @param string|Nano_C $controller
-	 * @param string $action
-	 */
-	public function invokeControllerAction($controller, $action) {
-		$instance = ($controller instanceof Nano_C) ? $controller : new $controller(Nano::dispatcher()); /* @var $instance Nano_C */
-		return $instance->run($action);
-	}
-
-	/**
-	 * @return string
-	 * @param string $controller
-	 * @param string $action
-	 * @param string $pattern
-	 * @param string $url
-	 */
-	public function runAction($controller, $action, $pattern = null, $url = null) {
-		$this->dispatcher->clean();
-		$route = Nano_Route::create($pattern, $controller, $action);
-		if (null !== $url) {
-			$this->dispatcher->test($route, $url);
-		}
-		return $this->dispatcher->run($route);
-	}
-
-	public function runModuleAction($module, $controller, $action, $pattern = null, $url = null) {
-		$this->dispatcher->clean();
-		$route = Nano_Route::create($pattern, $controller, $action, $module);
-		if (null !== $url) {
-			$this->dispatcher->test($route, $url);
-		}
-		return $this->dispatcher->run($route);
-	}
-
-	/**
-	 * @return stdClass
-	 * @param string $controller
-	 * @param string $action
-	 * @param string $pattern
-	 * @param string $url
-	 */
-	public function runJSONP($controller, $action, $pattern = null, $url = null) {
-		return $this->extractJSONP($this->runAction($controller, $action, $pattern, $url));
-	}
-
-	/**
-	 * @return stdClass
-	 * @param string $html
-	 */
-	public function extractJSONP($html) {
-		$data = $html;
-		$data = preg_replace('~^\s*<html>\s*<head>\s*<script type="text/javascript">\s*window\.name=\'~', '', $data);
-		$data = preg_replace('~\';\s*</script>\s*</head>\s*<body>\s*</body>\s*</html>\s*$~', '', $data);
-		return json_decode($data);
-	}
+//	/**
+//	 * @return string
+//	 * @param string|Nano_C $controller
+//	 * @param string $action
+//	 */
+//	public function invokeControllerAction($controller, $action) {
+//		$instance = ($controller instanceof Nano_C) ? $controller : new $controller(Nano::dispatcher()); /* @var $instance Nano_C */
+//		return $instance->run($action);
+//	}
+//
+//	/**
+//	 * @return string
+//	 * @param string $controller
+//	 * @param string $action
+//	 * @param string $pattern
+//	 * @param string $url
+//	 */
+//	public function runAction($controller, $action, $pattern = null, $url = null) {
+//		$this->dispatcher->clean();
+//		$route = Nano_Route::create($pattern, $controller, $action);
+//		if (null !== $url) {
+//			$this->dispatcher->test($route, $url);
+//		}
+//		return $this->dispatcher->run($route);
+//	}
+//
+//	public function runModuleAction($module, $controller, $action, $pattern = null, $url = null) {
+//		$this->dispatcher->clean();
+//		$route = Nano_Route::create($pattern, $controller, $action, $module);
+//		if (null !== $url) {
+//			$this->dispatcher->test($route, $url);
+//		}
+//		return $this->dispatcher->run($route);
+//	}
+//
+//	/**
+//	 * @return stdClass
+//	 * @param string $controller
+//	 * @param string $action
+//	 * @param string $pattern
+//	 * @param string $url
+//	 */
+//	public function runJSONP($controller, $action, $pattern = null, $url = null) {
+//		return $this->extractJSONP($this->runAction($controller, $action, $pattern, $url));
+//	}
+//
+//	/**
+//	 * @return stdClass
+//	 * @param string $html
+//	 */
+//	public function extractJSONP($html) {
+//		$data = $html;
+//		$data = preg_replace('~^\s*<html>\s*<head>\s*<script type="text/javascript">\s*window\.name=\'~', '', $data);
+//		$data = preg_replace('~\';\s*</script>\s*</head>\s*<body>\s*</body>\s*</html>\s*$~', '', $data);
+//		return json_decode($data);
+//	}
 
 	/**
 	 * @return void
@@ -80,8 +80,8 @@ class TestUtils_ControllerTestCase extends TestUtils_TestCase {
 		if ($this->clearDatabase) {
 			Nano_Db::clean();
 		}
-		$this->dispatcher = new Nano_Dispatcher();
-		Nano::helper()->setDispatcher($this->dispatcher);
+//		$this->dispatcher = new Nano_Dispatcher();
+//		Nano::helper()->setDispatcher($this->dispatcher);
 	}
 
 	protected function tearDown() {
@@ -89,7 +89,7 @@ class TestUtils_ControllerTestCase extends TestUtils_TestCase {
 			Nano_Db::clean();
 			Nano_Db::close();
 		}
-		Nano::helper()->setDispatcher(null);
+//		Nano::helper()->setDispatcher(null);
 	}
 
 }
